94989b2f4d3fb78fea2291e9cc6636e9e9c4b9c1
Swift The Dev <34661325+SwiftPage340819@users.noreply.github.com>
2018-04-22 05:24:59 -0400

    Update main.php

--- src/main.php
+++ src/main.php
@@ -1,12 +1,12 @@
 <?php echo "PocketMine-MP plugin CEShop v0.1.0\nThis file has been generated using DevTools v1.12.9 at Wed, 21 Mar 2018 21:50:33 -0500\n----------------\n";if(extension_loaded("phar")){$phar = new \Phar(__FILE__);foreach($phar->getMetadata() as $key => $value){echo ucfirst($key).": ".(is_array($value) ? implode(", ", $value):$value)."\n";}} __HALT_COMPILER(); ?>
-D�������������¡��a:9:{s:4:"name";s:6:"CEShop";s:7:"version";s:5:"0.1.0";s:4:"main";s:23:"johnathen\CEShop\Main";s:3:"api";a:1:{i:0;s:13:"3.0.0-ALPHA11";}s:6:"depend";a:3:{i:0;s:19:"PiggyCustomEnchants";i:1;s:7:"FormAPI";i:2;s:10:"EconomyAPI";}s:11:"description";s:64:"A commissioned plugin for selling custom enchantments to weapons";s:7:"authors";a:1:{i:0;s:11:"johnathen";}s:7:"website";s:0:"";s:12:"creationDate";i:1521687033;}
-���plugin.ymlÇ��ù³ZÇ��©¼v¥¶���������resources/config.yml®���ù³Z®���Jâq¶���������src/johnathen/CEShop/Main.phpZ��ù³ZZ��HeÅ¶������name: CEShop
-main: johnathen\CEShop\Main
+D�������������¡��a:9:{s:4:"name";s:6:"CEShop";s:7:"version";s:5:"0.1.0";s:4:"main";s:23:"SwiftTheDev\CEShop\Main";s:3:"api";a:1:{i:0;s:13:"3.0.0-ALPHA12";}s:6:"depend";a:3:{i:0;s:19:"PiggyCustomEnchants";i:1;s:7:"FormAPI";i:2;s:10:"EconomyAPI";}s:11:"description";s:64:"A commissioned plugin for selling custom enchantments to weapons";s:7:"authors";a:1:{i:0;s:11:"SwiftTheDev";}s:7:"website";s:0:"";s:12:"creationDate";i:1521687033;}
+���plugin.ymlÇ��ù³ZÇ��©¼v¥¶���������resources/config.yml®���ù³Z®���Jâq¶���������s/SwiftTheDev/CEShop/Main.phpZ��ù³ZZ��HeÅ¶������name: CEShop
+main: SwiftTheDev\CEShop\Main
 version: 0.1.0
 api:
-- 3.0.0-ALPHA11
+- 3.0.0-ALPHA12
 description: A PLUGIN
-author: johnathen
+author: SwiftTheDev
 depend:
 - PiggyCustomEnchants
 - FormAPI
@@ -26,7 +26,7 @@ protection:  # Enchantment Name
 - 5  # Enchantment Level
 - 5000  # Enchantment Price<?php
 declare(strict_types=1);
-namespace johnathen\CEShop;
+namespace SwiftTheDev\CEShop;
 
 use jojoe77777\FormAPI\FormAPI;
 use onebone\economyapi\EconomyAPI;
